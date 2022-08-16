790011320474440fbb4fe4d9709c954299fc54b6
Peter Buzanits <p.buzanits@epb.at>
2022-08-16 20:11:44 +0200

    initial commit

---
+++ src/Kubernetes.php
@@ -0,0 +1,39 @@
+<?php
+namespace booosta\kubernetes;o
+
+use RenokiCo\PhpK8s\KubernetesCluster;
+
+use \booosta\Framework as b;
+b::init_module('kubernetes');
+
+
+class Kubernetes extends \booosta\base\Module
+{ 
+  use moduletrait_kubernetes;
+
+  protected $url;
+  protected $token;
+
+
+  public function __construct($url, $token)
+  {
+    parent::__construct();
+
+    $this->cluster = KubernetesCluster::fromUrl($url);
+    $this->cluster->withoutSslChecks();
+
+    $this->cluster->withToken($token);
+  }
+
+  public function apply($code)
+  {
+    $yaml = $cluster->fromYaml($code);
+    $yaml->createOrUpdate();
+  }
+
+  public function delete($code)
+  {
+    $yaml = $cluster->fromYaml($code);
+    $yaml->delete()
+  }
+}
